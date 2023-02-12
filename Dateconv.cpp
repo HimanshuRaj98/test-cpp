5353b2f294b592262f59492d82a107f79cffcd90
Himanshu Raj <himanshurj85@gmail.com>
2023-02-12 23:47:51 +0530

    Create Dateconv.cpp

---
+++ Dateconv.cpp
@@ -0,0 +1,18 @@
+#include <ctime>
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+int main()
+{
+  ostringstream date1;
+  date1 << 20211119; //Date in YYMMDD format
+
+  struct tm tm;
+  strptime(date1.str().c_str(), "%Y%m%d", &tm);
+
+  char date2[50];
+  strftime(date2, sizeof(date2), "%d-%b-%Y", &tm);
+
+  std::cout << date1.str() << " -> " << date2 << std::endl;
+}
