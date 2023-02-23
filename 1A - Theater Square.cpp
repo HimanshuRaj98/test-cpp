baec017b7e6ef3cac59f34dd01532df05c1349ce
Himanshu Raj <himanshurj85@gmail.com>
2023-02-23 23:58:07 +0530

    Update 1A - Theater Square.cpp

--- 1A - Theater Square.cpp
+++ 1A - Theater Square.cpp
@@ -5,8 +5,9 @@ using namespace std;
 int main()
 {
     long long n, m, a, number;
+    const int size = 1e6 + 2 ;
     cin >> n >> m >> a;
     number = ((n + a - 1)) / a * ((m + a - 1) / a);
-    cout << number << endl;
+    cout << number << " " <<size << endl;
     return 0;
 }
